dbe85b34028cc3322d0ce153b984e30333e1cd26
Manyo <agusmangini@hotmail.com>
2022-10-13 22:02:47 -0300

    Multiples mazos y mezcla

--- api/services/baraja.service.ts
+++ api/services/baraja.service.ts
@@ -2,7 +2,21 @@ import { Carta } from "../models/carta";
 
 export class BarajaService {
 
-    prepararMazo(): Carta[] {
+    mezclarFisherYates(mazo: Carta[]): Carta[] {
+        let indice: number = mazo.length
+        let rand: number;
+        while (indice != 0) {
+          rand = Math.floor(Math.random() * indice);
+          indice--;
+
+          [mazo[indice], mazo[rand]] = [
+            mazo[rand], mazo[indice]];
+        }
+      
+        return mazo;
+    }
+      
+    private prepararMazo(): Carta[] {
         let mazo: Carta[] = [];
         for (let p = 0; p < 4; p++) {
             for (let numValor = 1; numValor < 14; numValor++) {
@@ -49,4 +63,12 @@ export class BarajaService {
         return mazo;
     }
 
+    prepararVariosMazos(n: number): Carta[]{
+        let muchosMazos: Carta[] = [];
+        for (let i = 0; i < n; i++) {
+            muchosMazos.concat(this.prepararMazo());
+        }
+        let mazoFinal: Carta[] = this.mezclarFisherYates(muchosMazos);
+        return mazoFinal;
+    }
 }  
