a30a4d250d18e898c6a17e615712518e6baa84d4
Abraguas <fmoises2002@gmail.com>
2022-10-14 22:01:36 -0300

    Refactorizo barajador para que no sea una clase

--- src/services/barajador.service.ts
+++ src/services/barajador.service.ts
@@ -1,74 +1,73 @@
 import { Carta } from "../models/carta";
 
-export class BarajadorService {
 
-    mezclarFisherYates(mazo: Carta[]): Carta[] {
-        let indice: number = mazo.length
-        let rand: number;
-        while (indice != 0) {
-          rand = Math.floor(Math.random() * indice);
-          indice--;
 
-          [mazo[indice], mazo[rand]] = [
-            mazo[rand], mazo[indice]];
-        }
+export function mezclarFisherYates(mazo: Carta[]): Carta[] {
+  let indice: number = mazo.length
+  let rand: number;
+  while (indice != 0) {
+    rand = Math.floor(Math.random() * indice);
+    indice--;
+
+    [mazo[indice], mazo[rand]] = [
+      mazo[rand], mazo[indice]];
+  }
+
+  return mazo;
+}
       
-        return mazo;
-    }
-      
-    private prepararMazo(): Carta[] {
-        let mazo: Carta[] = [];
-        for (let p = 0; p < 4; p++) {
-            for (let numValor = 1; numValor < 14; numValor++) {
-                switch (numValor) {
-                    case 11:
-                      mazo.push({
-                        valorCarta: 'J',
-                        valorNumerico: 10,
-                        palo: p
-                      });
-                      break;
-                    case 12:
-                      mazo.push({
-                        valorCarta: 'Q',
-                        valorNumerico: 10,
-                        palo: p
-                      });
-                      break;
-                    case 13:
-                      mazo.push({
-                        valorCarta: 'K',
-                        valorNumerico: 10,
-                        palo: p
-                      });
-                      break;
-                    case 1:
-                      mazo.push({
-                        valorCarta: 'A',
-                        valorNumerico: 11,
-                        palo: p
-                      });
-                      break;
-              
-                    default:
-                      mazo.push({
-                        valorCarta: numValor.toString(),
-                        valorNumerico: numValor,
-                        palo: p
-                      });
-                      break;
-                  }   
-            }
-        }
-        return mazo;
-    }
+export function prepararMazo(): Carta[] {
+  let mazo: Carta[] = [];
+  for (let p = 0; p < 4; p++) {
+    for (let numValor = 1; numValor < 14; numValor++) {
+      switch (numValor) {
+        case 11:
+          mazo.push({
+            valorCarta: 'J',
+            valorNumerico: 10,
+            palo: p
+          });
+          break;
+        case 12:
+          mazo.push({
+            valorCarta: 'Q',
+            valorNumerico: 10,
+            palo: p
+          });
+          break;
+        case 13:
+          mazo.push({
+            valorCarta: 'K',
+            valorNumerico: 10,
+            palo: p
+          });
+          break;
+        case 1:
+          mazo.push({
+            valorCarta: 'A',
+            valorNumerico: 11,
+            palo: p
+          });
+          break;
 
-    prepararVariosMazos(n: number): Carta[]{
-        let muchosMazos: Carta[] = [];
-        for (let i = 0; i < n; i++) {
-          muchosMazos.concat(this.prepararMazo());
-        }
-        let mazoFinal: Carta[] = this.mezclarFisherYates(muchosMazos);
-        return mazoFinal;
+        default:
+          mazo.push({
+            valorCarta: numValor.toString(),
+            valorNumerico: numValor,
+            palo: p
+          });
+          break;
+      }
     }
+  }
+  return mazo;
+}
+
+export function prepararVariosMazos(n: number): Carta[]{
+  let muchosMazos: Carta[] = [];
+  for (let i = 0; i < n; i++) {
+    muchosMazos.concat(prepararMazo());
+  }
+  let mazoFinal: Carta[] = mezclarFisherYates(muchosMazos);
+  return mazoFinal;
 }
