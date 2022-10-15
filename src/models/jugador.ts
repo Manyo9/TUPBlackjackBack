4484571f5fee16ae0eb57573e7a00146aae06315
Manyo <agusmangini@hotmail.com>
2022-10-15 18:28:58 -0300

    Agrego logica en jugador

--- src/models/jugador.ts
+++ src/models/jugador.ts
@@ -18,4 +18,46 @@ export class Jugador {
         this.terminoJugada = terminoJugada;
         this.perdio = perdio;
     }
+
+    agregarCarta(carta: Carta): void{
+        this.mano.push(carta);
+        this.chequearCondicion();
+        this.puntos = this.calcularPuntos();
+    }
+
+    calcularPuntos(): number{
+        let acumulador: number = 0;
+        this.mano.forEach(carta => {
+            acumulador += carta.valorNumerico; 
+        });
+        return acumulador;
+    }
+
+    chequearPlantadoForzado() : boolean {
+        if(this.esCroupier){
+            return this.calcularPuntos() > 16;
+        } else {
+            return this.calcularPuntos() === 21;
+        }
+    }
+
+    chequearCondicion(): void {
+        if(this.calcularPuntos() > 21){
+          for (let carta of this.mano) {
+            if (carta.valorNumerico === 11){
+                carta.valorNumerico = 1;
+                this.terminoJugada = this.chequearPlantadoForzado();  //Failsafe por si la ultima carta pedida es un as
+                return;
+              }    
+            }
+            this.perdio = true;
+            this.terminoJugada = true;
+        } else if (this.chequearPlantadoForzado()) {
+            this.terminoJugada = true;
+        }
+    }
+
+    plantar(): void {
+        this.terminoJugada = true;
+    }
 }
