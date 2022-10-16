f3b7bfff63ee14b1112b5f0b71a4784dbdaabfd6
Abraguas <fmoises2002@gmail.com>
2022-10-16 04:39:10 -0300

    Agrego dependencias y uso de cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import usuariosRouter from './routes/usuarios';
 import partidasRouter from './routes/partidas'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
