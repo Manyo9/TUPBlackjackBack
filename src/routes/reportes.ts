2ebd577d39b961338f7542130199ee9ca6d4e786
Abraguas <fmoises2002@gmail.com>
2022-11-09 19:55:24 -0300

    Rutas y consultas de reportes

--- src/routes/reportes.ts
+++ src/routes/reportes.ts
@@ -30,6 +30,81 @@ router.get('/indiceResultados', verifyToken, (req: any, res: any) => {
     }
 })
 
+router.get('/promedioVeintiuno', verifyToken, (req: any, res: any) => {
+    if (!req.data) {
+        res.status(401).json({ "ok": false, "mensaje": "Token inválido." });
+        return;
+    }
+    if (req.data.rol == 'admin') {
+        reporteService.obtenerPromedioVeintiuno()
+        .catch((e) => {
+            res.status(500).json({
+                "ok": false,
+                "mensaje": "Error en la db"
+            });
+            console.error(e);
+        })
+        .then((x) => {
+            console.log(x);
+            const response = {
+                jugadores: x[1][0].jugadores,
+                croupier :  x[1][0].croupier
+            }
+            res.status(200).json({ "ok": true, "resultado": response });
+        })
+        
+    } else {
+        res.status(403).json({ "ok": false, "mensaje": "Usted no tiene los permisos requeridos para acceder a este recurso." });
+    }
+})
+
+router.get('/cantidadJuegosJugadores', verifyToken, (req: any, res: any) => {
+    if (!req.data) {
+        res.status(401).json({ "ok": false, "mensaje": "Token inválido." });
+        return;
+    }
+    if (req.data.rol == 'admin') {
+        reporteService.obtenerCantidadJuegosJugadores()
+        .catch((e) => {
+            res.status(500).json({
+                "ok": false,
+                "mensaje": "Error en la db"
+            });
+            console.error(e);
+        })
+        .then((x) => {
+            x = x[0];
+            res.status(200).json({ "ok": true, "resultado": x });
+        })
+        
+    } else {
+        res.status(403).json({ "ok": false, "mensaje": "Usted no tiene los permisos requeridos para acceder a este recurso." });
+    }
+})
+router.get('/cantidadVictoriasUsuario', verifyToken, (req: any, res: any) => {
+    if (!req.data) {
+        res.status(401).json({ "ok": false, "mensaje": "Token inválido." });
+        return;
+    }
+    if (req.data.rol == 'admin') {
+        reporteService.obtenerCantidadVictoriasUsuario()
+        .catch((e) => {
+            res.status(500).json({
+                "ok": false,
+                "mensaje": "Error en la db"
+            });
+            console.error(e);
+        })
+        .then((x) => {
+            x = x[0];
+            res.status(200).json({ "ok": true, "resultado": x });
+        })
+        
+    } else {
+        res.status(403).json({ "ok": false, "mensaje": "Usted no tiene los permisos requeridos para acceder a este recurso." });
+    }
+})
+
 function verifyToken(req: any, res: any, next: any) {
     if (!req.headers.authorization) return res.status(401).json({ "ok": false, "mensaje": "No autorizado" });
     let token = req.headers.authorization.split(' ')[1];
--- src/services/reporte.service.ts
+++ src/services/reporte.service.ts
@@ -13,3 +13,29 @@ export const obtenerIndiceResultados = (): Promise<any> => {
         })
       })
 }
+
+export const obtenerPromedioVeintiuno = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
+      mysqlConnecction.query('call spPromedioVentiuno(@c, @j); select @c as croupier, @j as jugadores;', (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+      })
+    })
+}
+export const obtenerCantidadVictoriasUsuario = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
+      mysqlConnecction.query('call spCantidadVictoriasUsuario()', (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+      })
+    })
+}
+
+export const obtenerCantidadJuegosJugadores = (): Promise<any> => {
+  return new Promise((resolve, reject) => {
+      mysqlConnecction.query('call spCantidadJuegosJugadores()', (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+      })
+    })
+}
