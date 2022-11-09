2c691cfe2666171148fa63775486ecebd4a71c95
Manyo <agusmangini@hotmail.com>
2022-11-08 23:15:49 -0300

    primer reporte con datos de prueba

--- db/creacion.sql
+++ db/creacion.sql
@@ -1,6 +1,12 @@
 Create database blackjack;
 use blackjack;
 
+create table estadoGanador (
+	id tinyint auto_increment,
+    descripcion varchar(10),
+	primary key (id)
+);
+
 create table Roles(
     id int auto_increment,
     nombre varchar(30),
@@ -16,17 +22,24 @@ create table Usuarios(
     foreign key (idRol) references Roles(id)
 );
 
+create table Partidas(
+	id int auto_increment,
+    idUsuario int,
+    primary key (id),
+    foreign key (idUsuario) references Usuarios(id)
+);
+
 create table Resultados(
     id int auto_increment,
     idPartida int,
-	idUsuario int,
-    idGanador tinyint,
+    idEstadoGanador tinyint,
     puntajeCroupier tinyint,
     puntajeJugador tinyint,
 	fechaFinalizacion datetime,
     primary key (id),
-    foreign key (idUsuario) references Usuarios(id)
+    foreign key (idPartida) references Partidas(id)
 );
+
 DELIMITER //
 
 CREATE PROCEDURE spIniciarSesion(
@@ -58,10 +71,22 @@ BEGIN
     where u.id = id;
 END //
 
-CREATE PROCEDURE spIndiceVictorias(
+CREATE PROCEDURE spSiguienteIdPartida(
 )
 BEGIN
-	
+	select id + 1 as nextId from partidas order by 1 desc limit 1;
+END //
+
+CREATE PROCEDURE spIndiceResultados(
+	OUT victoriasCroupier int,
+    OUT victoriasJugador int,
+    OUT empates int
+)
+BEGIN
+	select r.id, r.idPartida, e.descripcion as ganador,
+    r.puntajeCroupier, r.puntajeJugador, r.fechaFinalizacion
+    from resultados r 
+    join estadoGanador e on r.idEstadoGanador = e.id;
 END //
 
 DELIMITER ;
